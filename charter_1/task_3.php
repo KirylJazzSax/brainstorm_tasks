906a9035d26e2c5fc34bac5e19ff8a6746c54804
KirylJazzSax <matsak_92@mail.ru>
2020-02-12 14:43:24 +0300

    Обнаружил ошибку

--- charter_1/task_3.php
+++ charter_1/task_3.php
@@ -6,18 +6,18 @@ function isIncreasing($n)
     $n = (int)$n;
 
     while(true) {
-        if ($n == 0) {
-            return true;
-        }
+        if ($n < 1) return true;
 
         $lastNumber = $n % 10;
-        $n = $n / 10;
-        $beforeLastNumber = ($n % 10);
+        $n = (int)$n / 10;
+        $beforeLastNumber = $n % 10;
 
-        if ($beforeLastNumber > $lastNumber) return false;
+        echo $lastNumber . ' ' . $beforeLastNumber . ' ' . PHP_EOL;
+
+        if ($beforeLastNumber >= $lastNumber) return false;
     }
 }
 
-echo isIncreasing(23458)
+echo isIncreasing(123458)
     ? 'Числа идут по возрастанию' . PHP_EOL
     : 'Числа не идут по возрастанию или число отрицательное' . PHP_EOL;
