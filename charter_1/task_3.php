b5568309abae7c969b4a51563000c4f8d7046e28
KirylJazzSax <matsak_92@mail.ru>
2020-02-12 14:07:44 +0300

    Задание 3

---
+++ charter_1/task_3.php
@@ -0,0 +1,23 @@
+<?php
+
+function isIncreasing($n)
+{
+    if ($n < 0) return false;
+    $n = (int)$n;
+
+    while(true) {
+        if ($n == 0) {
+            return true;
+        }
+
+        $lastNumber = $n % 10;
+        $n = $n / 10;
+        $beforeLastNumber = ($n % 10);
+
+        if ($beforeLastNumber > $lastNumber) return false;
+    }
+}
+
+echo isIncreasing(23458)
+    ? 'Числа идут по возрастанию' . PHP_EOL
+    : 'Числа не идут по возрастанию или число отрицательное' . PHP_EOL;
