043fb215050aedf2791ff9ac4fbf1002dab5f127
KirylJazzSax <matsak_92@mail.ru>
2020-02-12 17:26:56 +0300

    Задание 7

---
+++ charter_1/task_7.php
@@ -0,0 +1,23 @@
+<?php
+
+function compareNumber(int $n, int $compareValue): bool
+{
+    while (true) {
+        if ($n < 1) return false;
+        echo $compareValue . ' ' . ($n % 10) . ' ' . PHP_EOL;
+        if ($compareValue == $n % 10) return true;
+        $n = (int)($n / 10);
+    }
+}
+
+function hasSameNumbers($n): bool
+{
+    if ($n < 0) return false;
+    while (true) {
+        if ($n < 1) return false;
+        if (compareNumber((int)($n / 10), $n % 10)) return true;
+        $n = (int)($n / 10);
+    }
+}
+
+echo hasSameNumbers(134556789) ? "Есть\n" : "Нет\n";
