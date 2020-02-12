165c6501c41f4fa430db41f4a41efdd0502d9de7
KirylJazzSax <matsak_92@mail.ru>
2020-02-12 15:26:39 +0300

    Задание 4

---
+++ charter_1/task_4.php
@@ -0,0 +1,34 @@
+<?php
+
+function increaseOrDecrease($n)
+{
+    if ($n < 0) return false;
+    $n = (int)$n;
+    $increase = (int)(($n % 100) / 10) < $n % 10 ? 1 : 0;
+
+    while(true) {
+        if ($n < 1) return true;
+
+        $lastNumber = $n % 10;
+        $n = (int)$n / 10;
+        $beforeLastNumber = $n % 10;
+
+        if ($increase == 1) {
+            if ($beforeLastNumber >= $lastNumber) return false;
+        } else {
+            if ($beforeLastNumber <= $lastNumber && $n >= 1) return false;
+        }
+    }
+}
+
+function getEvenFourDigitsNumber()
+{
+    $i = 1000;
+
+    while ($i < 10000) {
+        if (increaseOrDecrease($i) && $i % 2 == 0) echo $i . PHP_EOL;
+        $i++;
+    }
+}
+
+getEvenFourDigitsNumber();
