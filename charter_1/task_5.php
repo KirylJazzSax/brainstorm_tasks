7a1de4b685db5f1d315cfe84c423cd3f3269c476
KirylJazzSax <matsak_92@mail.ru>
2020-02-12 15:55:25 +0300

    Задание 5

---
+++ charter_1/task_5.php
@@ -0,0 +1,16 @@
+<?php
+
+function revertNumber(int $n)
+{
+    if ($n < 0) return false;
+
+    $m = null;
+
+    while (true) {
+        if ($n < 1) return (int)$m;
+        $m .= $n % 10;
+        $n = (int)($n / 10);
+    }
+}
+
+echo revertNumber(123456789) . PHP_EOL;
