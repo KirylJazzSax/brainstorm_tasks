0d8674d466b9b1d1d26bc515663fd6bda06f85d2
KirylJazzSax <matsak_92@mail.ru>
2020-02-12 17:43:42 +0300

    Задание 8

---
+++ charter_1/task_8.php
@@ -0,0 +1,31 @@
+<?php
+
+function compareNumber(int $n, int $compareValue): bool
+{
+    while (true) {
+        if ($n < 1) return false;
+        if ($compareValue == $n % 10) return true;
+        $n = (int)($n / 10);
+    }
+}
+
+function hasSameNumbers(int $n): bool
+{
+    if ($n < 0) return false;
+    while (true) {
+        if ($n < 1) return false;
+        if (compareNumber((int)($n / 10), $n % 10)) return true;
+        $n = (int)($n / 10);
+    }
+}
+
+function printNumbersWithoutSameDigits()
+{
+    $i = 1000;
+    while ($i < 10000) {
+        if (!hasSameNumbers($i)) echo $i .' ' . -$i . PHP_EOL;
+        $i++;
+    }
+}
+
+printNumbersWithoutSameDigits();
