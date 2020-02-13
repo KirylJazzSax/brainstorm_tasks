51e1ad28735e1e771936a17d83b1d27dbde5d51f
KirylJazzSax <matsak_92@mail.ru>
2020-02-13 12:10:24 +0300

    Залание 12

---
+++ charter_1/task_12.php
@@ -0,0 +1,18 @@
+<?php
+
+function isSimple(int $n): bool
+{
+    if ($n < 0) return false;
+    if ($n == 1 || $n == 2) return true;
+
+    if ($n % 1 == 0) {
+        $i = 3;
+        while ($i < $n) {
+            if ($i == $n - 1) return true;
+            if ($n % $i == 0) return false;
+            $i++;
+        }
+    }
+}
+
+echo isSimple(7) ? "yes\n": "no\n";
