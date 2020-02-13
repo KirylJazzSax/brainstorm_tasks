a660d64bd96aaac1154f86229f5427e471816287
KirylJazzSax <matsak_92@mail.ru>
2020-02-13 09:54:37 +0300

    Залание 9

---
+++ charter_1/task_9.php
@@ -0,0 +1,16 @@
+<?php
+
+function isAutomorphic(int $n, $previous = null): bool
+{
+    $number = $n;
+    $lastDigit = null;
+
+    while (true) {
+        if ($n == 0) return false;
+        $lastDigit = $lastDigit ? (int)(($n % 10) . $lastDigit) : $n % 10;
+        if ((int)($number / $lastDigit) == $lastDigit) return true;
+        $n = (int)($n / 10);
+    }
+}
+
+echo isAutomorphic(87909376) ? "Автоморфное\n" : "Не автоморфное\n";
