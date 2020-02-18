2a05423f335b41d6aa1515fc5c9c26c957e39434
KirylJazzSax <matsak_92@mail.ru>
2020-02-18 15:13:58 +0300

    Глава 2 Задание 22

---
+++ charter_2/task_22.php
@@ -0,0 +1,116 @@
+<?php
+
+function arrayLength(array $arr): int
+{
+    $i = 0;
+    while (true) {
+        if (!isset($arr[$i])) return $i;
+        $i++;
+    }
+}
+
+/**
+ * @param array $arr
+ * @param $element
+ * @return bool|int элемент если найден в другом случае false
+ */
+function find(array $arr, $element)
+{
+    for ($i = 0; $i < arrayLength($arr); $i++) {
+        if ($arr[$i] == $element) return $element;
+    }
+    return false;
+}
+
+/**
+ * @param array $arr
+ * @param int $element
+ * @return int|bool index елемента если не нашёл то false
+ */
+function getIndexOf(array $arr, int $element)
+{
+    for ($i = 0; $i < arrayLength($arr); $i++) {
+        if ($arr[$i] == $element) return $i;
+    }
+    return false;
+}
+
+function arrayPush(array $array, $element): array
+{
+    $array[] = $element;
+    return $array;
+}
+
+function arrayFilter(array $arr, callable $func): array
+{
+    $result = [];
+    for ($i = 0; $i < arrayLength($arr); $i++) {
+        if ($func($arr[$i])) {
+            $result[] = $arr[$i];
+        }
+    }
+
+    return $result;
+}
+
+function arraySplice(array $array, int $index, int $amount = 1): array
+{
+    $result = [];
+
+    for ($i = 0; $i < arrayLength($array); $i++) {
+        if ($i == $index) {
+            $i += $amount - 1;
+            continue;
+        }
+        $result = arrayPush($result, $array[$i]);
+    }
+
+    return $result;
+}
+
+
+function getRepeatedElements(array $array): array
+{
+    $repeatedElementsArray = [];
+    for ($i = 0; $i < arrayLength($array); $i++) {
+
+        $repeatedElements = arrayFilter($array, function ($element) use ($array, $i) {
+            return $element == $array[$i] ? true : false;
+        });
+
+        if (arrayLength($repeatedElements) > 1 && !find($repeatedElementsArray, $repeatedElements[0])) {
+            $repeatedElementsArray = arrayPush($repeatedElementsArray, $repeatedElements[0]);
+        }
+    }
+    return $repeatedElementsArray;
+}
+
+function getUniqueElements(array $array): array
+{
+    $repeatedElements = getRepeatedElements($array);
+    for ($i = 0; $i < arrayLength($repeatedElements); $i++) {
+        while (getIndexOf($array, $repeatedElements[$i]) !== false) {
+            $array = arraySplice($array, getIndexOf($array, $repeatedElements[$i]));
+        }
+    }
+    return $array;
+}
+
+function getBiggestElementInArray(array $arr): int
+{
+    $max = $arr[0];
+    for ($i = 0; $i < arrayLength($arr); $i++) {
+        if ($max < $arr[$i]) $max = $arr[$i];
+    }
+    return $max;
+}
+
+#############################################################################################################
+
+$array = [12, 13, 12, -14, 25, 18, -19, 25, 20, -14, 13, 15, 15, 15, -20, 15, 15, 15];
+
+$biggestUniqueElement = getBiggestElementInArray(
+    getUniqueElements($array)
+);
+
+echo $biggestUniqueElement . PHP_EOL;
